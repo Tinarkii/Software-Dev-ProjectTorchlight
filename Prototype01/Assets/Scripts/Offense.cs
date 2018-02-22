2c0e594d2d42f557bad17fe2a3c9bcd861eeecf5
joelpenney <joel.penney@my.wheaton.edu>
2018-02-22 11:09:00 -0600

    Added control script for encounters

---
+++ Prototype01/Assets/Scripts/Offense.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Offense : MonoBehaviour {
+
+
+	public Boolean Finished() {
+		return true;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
