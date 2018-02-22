9632cdafc1745e4eb2ad58c9bce06ffe12b1af4f
DrewSmith343 <drew.smith@my.wheaton.edu>
2018-02-22 11:06:28 -0600

    Started retuning the movement system

--- Prototype01/Assets/Scripts/Physics.cs
+++ Prototype01/Assets/Scripts/Physics.cs
@@ -11,17 +11,33 @@ public class Physics : MonoBehaviour {
 	void Start () {
         target = new Vector3(0, 0, 0);
         self = GetComponent<Rigidbody>();
-
+		//tracked = new Dictionary<int, Vector3>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		/*Touch[] touches = Input.touches;
+		for (int i = 0; i < Input.touchCount; i++) {
+			if (touches [i].phase == TouchPhase.Began) {
+				Vector2 temp = new Vector2 () { x = touches [i].position.x, y = touches [i].position.y };
+				tracked.Add (touches [i].fingerId, temp);
+			}
+
+			if (touches [i].phase == TouchPhase.Canceled) {
+				tracked.Remove (touches [i].fingerId);
+			}
+
+			if (touches [i].phase == TouchPhase.Ended) {
+				Vector2 temp;
+				tracked.TryGetValue (touches [i].fingerId, out temp);//Needs error handling
+			}
+		}*/
 
         if (Input.GetMouseButton(0))
         {
             target.x = Input.mousePosition.x - (Screen.width/2);
-            target.y = - (Input.mousePosition.y - (Screen.height/2));
+            target.z = - (Input.mousePosition.y - (Screen.height/2));
 
         }
         else
@@ -29,12 +45,14 @@ public class Physics : MonoBehaviour {
             target = new Vector3(0, 0, 0);
         }
 
-        target.Normalize();
-        target.Scale(new Vector3(power, power, power));
-        
-        self.AddRelativeForce(target);
+		target.Normalize();
+		target.Scale(new Vector3(power, power, power));
 
-        Debug.Log(Input.mousePosition.ToString());
+		self.velocity = new Vector3(-target.x, self.velocity.y, target.z); 
+
+        //self.AddRelativeForce(target);
+
+        //Debug.Log(Input.mousePosition.ToString());
 
 
     }
