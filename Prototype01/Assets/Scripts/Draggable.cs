0cb0ae02a01a3072630ad2c4d88dc442e5c83bc9
Zach <>
2018-02-24 01:48:02 -0600

    Rudimentary drag and drop implementation along a path for combat

---
+++ Prototype01/Assets/Scripts/Draggable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public Transform ParentToReturnTo = null;
+	public Transform snapBack = null;
+
+	public void OnBeginDrag(PointerEventData eventData){
+		//Debug.Log ("OnBeginDrag");
+
+		snapBack = this.transform.parent;
+		ParentToReturnTo = this.transform.parent;
+		this.transform.SetParent (this.transform.parent.parent);
+	}
+	public void OnDrag(PointerEventData eventData){
+		//Debug.Log ("OnDrag");
+
+		this.transform.position = eventData.position;
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+	}
+	public void OnEndDrag(PointerEventData eventData){
+		//Debug.Log ("OnEndDrag");
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
+		this.transform.SetParent (ParentToReturnTo);
+	}
+}
