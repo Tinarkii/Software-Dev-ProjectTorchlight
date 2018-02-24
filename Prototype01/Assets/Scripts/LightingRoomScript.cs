fee20dda302ab065a9cfdb960bd8266997c6453f
Tinarkii <jeffrey.thompson@my.wheaton.edu>
2018-02-23 23:05:26 -0600

    Put in some assets for the encounter system (found in prefabs, EncounterItems). Also did some stuff with the lighting scene that gives us a bit more leeway in throwing stuff in there (cause why not right?)

---
+++ Prototype01/Assets/Scripts/LightingRoomScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingRoomScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Mathf.Abs(Input.mousePosition.x - Screen.width/2) > (Screen.width / 5))
+        {
+            transform.
+            transform.Translate(Mathf.Sign(Input.mousePosition.x - Screen.width / 2) * -0.08f,0,0,Space.World);
+        }
+		
+	}
+}
