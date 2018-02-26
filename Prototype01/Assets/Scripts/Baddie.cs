a536c0c628fdac1070315b5b2485cbff9096ded3
DrewSmith343 <drew.smith@my.wheaton.edu>
2018-02-26 16:57:47 -0600

    Working on scene transition and overworld control

---
+++ Prototype01/Assets/Scripts/Baddie.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Baddie : MonoBehaviour {
+
+	public SphereCollider collider; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter (Collider coll){
+		BroadcastMessage ("initateEncounter"); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		OnTriggerEnter (collider); 
+	}
+}
---
+++ Prototype01/Assets/Scripts/OverWorldControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OverWorldControl : MonoBehaviour {
+
+	public GameObject gameBaddie;
+	public Scene sampleEncounter; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+		
+	void initateEncounter(){
+		SceneManager.LoadScene("sampleEncounter"); 
+	}
+		
+
+	// Update is called once per frame
+	void Update () {
+		initateEncounter ();
+	}
+}
