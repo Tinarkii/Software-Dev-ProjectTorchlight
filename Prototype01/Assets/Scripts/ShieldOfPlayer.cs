e4d6d4379ce80d9569d5837cda8e49042158b249
Luke <luke.penney@my.wheaton.edu>
2018-02-26 17:33:11 -0600

    Created first version of ShieldOfPlayer

---
+++ Prototype01/Assets/Scripts/ShieldOfPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldOfPlayer : MonoBehaviour {
+
+	/**
+	 * @TODO: write this
+	 */
+    private void OnCollisionEnter (Collision col)
+    {
+		// The player's shield doesn't care about collisions unless they're with the enemy's blasts
+        if (col.gameObject.name != "BlastBad")
+			return;
+
+		Debug.Log("SheildOfPlayer has collided with a BlastBad");
+    }
+
+}
