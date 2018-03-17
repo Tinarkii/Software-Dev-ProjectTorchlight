2af08cd0f24ba5394aa9629e2e445306ebce5162
Luke Penney <luke@penneys.us>
2018-03-17 11:08:57 -0500

    Working in the ItemsMenu script to make buttons to use items in the inventory

--- Prototype01/Assets/Scripts/Inventory/ItemsMenu.cs
+++ Prototype01/Assets/Scripts/Inventory/ItemsMenu.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /**
  * A class to display and control the items menu
  */
 
 public class ItemsMenu : MonoBehaviour {
+
+	// Should run when the ItemsMenuCanvas is instantiated
+	public void Awake ()
+	{
+		Debug.Log ("Awake called in ItemsMenu script");
+
+		Button button = GameObject.Find ("ResumeButton").GetComponent<Button>();
+		button.GetComponentInChildren<Text>().text = "text changed";
+		button.onClick.AddListener(delegate { ResumeGame(); });
+	}
 
 	/**
 	 * Resume the game. Should be called by a button being pressed.
