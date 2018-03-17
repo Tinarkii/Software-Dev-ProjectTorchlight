98b6f5db9fcbc6b67022db1ee4c99feea487080f
Zach <>
2018-03-16 19:01:26 -0500

    More rework of offense

--- Prototype01/Assets/Scripts/Encounter/Attack.cs
+++ Prototype01/Assets/Scripts/Encounter/Attack.cs
@@ -8,29 +8,94 @@ public class Attack : MonoBehaviour {
 	public static string[] shape = new string[6];
 
 	public static bool finished;
-	public static bool attackEnabled;
+
+	GameObject[] circles = new GameObject[5];
+	GameObject One;
+	GameObject Two;
+	GameObject Three;
+	GameObject Four;
+	GameObject Five;
 
 	// Use this for initialization
 	void Start () 
 	{
+		// Initialize and constuct the circles
+
+		One = (GameObject)Instantiate(Resources.Load("Circle"));
+		One.transform.position = new Vector3(-2, 0.5f, 0);
+		One.GetComponent<Renderer>().material.color = Color.white;
+		One.name = "1";
+		circles[0] = One;
+
+		Two = (GameObject)Instantiate(Resources.Load("Circle"));
+		Two.transform.position = new Vector3(-2, 3, 0);
+		Two.GetComponent<Renderer>().material.color = Color.white;
+		Two.name = "2";
+		circles[1] = Two;			
+
+		Three = (GameObject)Instantiate(Resources.Load("Circle"));
+		Three.transform.position = new Vector3(0, 3, 0);
+		Three.GetComponent<Renderer>().material.color = Color.white;
+		Three.name = "3";
+		circles[2] = Three;
+
+		Four = (GameObject)Instantiate(Resources.Load("Circle"));
+		Four.transform.position = new Vector3(2, 3, 0);
+		Four.GetComponent<Renderer>().material.color = Color.white;
+		Four.name = "4";
+		circles[3] = Four;
+
+		Five = (GameObject)Instantiate(Resources.Load("Circle"));
+		Five.transform.position = new Vector3(2, 0.5f, 0);
+		Five.GetComponent<Renderer>().material.color = Color.white;
+		Five.name = "5";
+		circles[4] = Five;
 	}
 
 	public bool Finished() 
 	{
+		if(finished)
+		{
+			for (int i = 0; i<=4; i++)
+			{
+				// Make the circles invisible
+				circles[i].GetComponent<Renderer>().material.color = Color.clear;
+
+				// Disable the circle scripts
+				circles[i].GetComponent<MouseOverStuff>().enabled = false;
+
+				// Reset shape check
+				MouseOverStuff.box = false;
+				MouseOverStuff.tri = false;
+				MouseOverStuff.x = false;
+			}
+		}
 		return finished;
 	}
 
 	void OnEnable() 
 	{
-		attackEnabled = true;
+		// Unsets finished flag
 		finished = false;
-		GameObject[] GOs = GameObject.FindGameObjectsWithTag("Circles");
-		for (int i = 0; i<GOs.Length; i++)
+
+		// Enable the scripts in circles
+		for (int i = 0; i<=4; i++)
 		{
-     		GOs[i].GetComponent<MouseOverStuff>().enabled = true;
+     		circles[i].GetComponent<MouseOverStuff>().enabled = true;
+			circles[i].GetComponent<Renderer>().material.color = Color.white;
  		}
 	}
 	
+	// Sets all values in the shape array to null
+	void ClearShape()
+	{
+		for(int i=0; i < 6; i++)
+		{
+			shape[i] = null;
+		}
+	}
+
+	// I will create the exit function later...
 	public bool ToExit() 
 	{
 		return false;
@@ -38,10 +103,38 @@ public class Attack : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () 
-	{
-		if (MouseOverStuff.box || MouseOverStuff.x || MouseOverStuff.tri)
+	{	
+		// Check if a shape has been drawn
+		if (MouseOverStuff.box) 
+		{
+			ClearShape();
+			Attack.touched = 0;
+			print("That's a box.");
 			finished = true;
-		if (finished)
-			attackEnabled = false;
+		}
+		else if (MouseOverStuff.x) 
+		{
+			ClearShape();
+			Attack.touched = 0;
+			print("That's an X.");
+			finished = true;
+		}
+		else if (MouseOverStuff.tri) 
+		{
+			ClearShape();
+			Attack.touched = 0;
+			print("That's a triangle.");
+			finished = true;
+		}
+		else if(touched>=6)
+		{
+			ClearShape();
+			touched = 0;
+			print("That's a not a shape.");
+			for (int i = 0; i<=4; i++)
+			{
+				circles[i].GetComponent<Renderer>().material.color = Color.white;
+			}
+		}	
 	}
 }
--- Prototype01/Assets/Scripts/Encounter/MouseOverStuff.cs
+++ Prototype01/Assets/Scripts/Encounter/MouseOverStuff.cs
@@ -4,39 +4,19 @@ using System;
 using UnityEngine;
 
 public class MouseOverStuff : MonoBehaviour {
-	public MouseOverStuff mos;
 	public static bool box;
 	public static bool x;
 	public static bool tri;
 	private Color startcolor;
 	private int i;
-	// Use this for initialization
 	void Start () 
 	{
-		mos = this.GetComponent<MouseOverStuff>();
-		startcolor = GetComponent<Renderer>().material.color;
-		GetComponent<Renderer>().material.color = Color.white;
 	}
-	/*public void AttackEnabled () 
-	{
-		GetComponent<Renderer>().material.color = Color.white;
-	}*/
 
 
  	void OnMouseEnter()
 	{
-		/* To prevent this functionality when script is disabled
-		 */
-		if (Attack.attackEnabled)
-		{
-			mos.enabled = true;
-			GetComponent<Renderer>().material.color = startcolor;
-		}
-		else if (!mos.enabled)
-		{
-			return;
-		}
-		//startcolor = GetComponent<Renderer>().material.color;
+
 		GetComponent<Renderer>().material.color = Color.yellow;	
 		Attack.shape[Attack.touched] = this.name;
 		Attack.touched = Attack.touched + 1;
@@ -44,69 +24,20 @@ public class MouseOverStuff : MonoBehaviour {
 		box = string.Join("", Attack.shape).CompareTo("123451")==0;
 		x = string.Join("", Attack.shape).CompareTo("14325")==0;
 		tri = string.Join("", Attack.shape).CompareTo("1351")==0;
-		if (box) 
-		{
-			for(i=0; i < 6; i++)
-			{
-				Attack.shape[i] = null;
-			}
-			Attack.touched = 0;
-			print("That's a box.");
-		}
-		else if (x) 
-		{
-			for(i=0; i < 6; i++)
-			{
-				Attack.shape[i] = null;
-			}
-			Attack.touched = 0;
-			print("That's an X.");
-		}
-		else if (tri) 
-		{
-			for(i=0; i < 6; i++)
-			{
-				Attack.shape[i] = null;
-			}
-			Attack.touched = 0;
-			print("That's a triangle.");
-		}
-		else if(Attack.touched>=6)
-		{
-			for(i=0; i < 6; i++)
-			{
-				Attack.shape[i] = null;
-			}
-			Attack.touched = 0;
-			print("That's a not a shape.");
-			GetComponent<Renderer>().material.color = Color.white;
-		}	
 	}
+
+
 
 	void OnEnable() 
 	{
-		GetComponent<Renderer>().material.color = Color.white;
 	}
 
  	void OnMouseExit()
- 	{
-		 if (mos.enabled)
-		 {
-			 //GetComponent<Renderer>().material.color = startcolor;
-		 }
-  		//GetComponent<Renderer>().material.color = startcolor;
+ 	{	
 	}
+
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Attack.finished)
-		{
-			GetComponent<Renderer>().material.color = Color.clear;
-			mos.enabled = false;
-			box = false;
-			x = false;
-			tri = false;
-		}
-
 	}
 }
