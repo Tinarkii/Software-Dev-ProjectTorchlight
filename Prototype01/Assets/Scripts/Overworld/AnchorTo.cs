3e3d7d79bf56365d7c02be4cb78e0e52c4470861
Tinarkii <jeffrey.thompson@my.wheaton.edu>
2018-03-17 17:22:39 -0500

    Better Nav stuff

---
+++ Prototype01/Assets/Scripts/Overworld/AnchorTo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchorTo : MonoBehaviour {
+	public GameObject target;
+	public Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		transform.position = target.transform.position + offset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = target.transform.position + offset;
+	}
+}
---
+++ Prototype01/Assets/Scripts/Overworld/OverworldCameraMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldCameraMovement : MonoBehaviour {
+
+	Rigidbody self;
+	public Rigidbody boy;
+	Vector3 target;
+	public int maxSpeed;
+	public int minSpeed;
+
+	// Use this for initialization
+	void Start () {
+		target = new Vector3(0, 0, 0);
+		self = GetComponent<Rigidbody>();
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		target = boy.transform.position;
+
+		target.z += 45;
+
+		target += (boy.transform.forward * 7 + boy.velocity);
+
+		target.y = boy.transform.position.y + 60;
+
+		Vector3 veloc = (target - self.position);
+
+		float speed = Mathf.Min (maxSpeed, veloc.magnitude);
+
+		veloc = (veloc.normalized);
+		veloc *= speed;
+
+		self.velocity = veloc;
+
+	}
+}
