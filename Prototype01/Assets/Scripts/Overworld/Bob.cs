438397ebe1a0d505936a918e1eb7f1206d9a16f1
Luke Penney <luke@penneys.us>
2018-03-23 16:50:53 -0500

    Added script to make crystalBaddie bob up and down

---
+++ Prototype01/Assets/Scripts/Overworld/Bob.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * A class for objects that bob up and down
+ */
+
+public class Bob : MonoBehaviour
+{
+	public Rigidbody rigidBody;
+	public float magnitude;
+	public float rate;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		rigidBody.velocity = new Vector3 (rigidBody.velocity.x, magnitude*Mathf.Sin(Time.time*rate), rigidBody.velocity.z);
+	}
+}
