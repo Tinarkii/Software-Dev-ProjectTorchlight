0d8bbc109f5627c8c36d8f3571f22a9537a605ec
zachjvandyke <zachjvandyke@gmail.com>
2018-04-24 09:18:24 -0500

    I wrote an implementation of click on lamp to save, but there are no lamps to test it on as SceneControl is broke.

---
+++ Prototype01/Assets/Prefabs/LampSave.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampSave : MonoBehaviour {
+	public GameObject dialogueBoxUIObject;
+	void Start()
+	{
+		dialogueBoxUIObject = GameObject.FindGameObjectWithTag("dialogueBoxUIObject");
+		dialogueBoxUIObject.SetActive(false);
+	}
+	void OnMouseDown()
+	{
+		dialogueBoxUIObject.SetActive(true);
+		Time.timeScale = 0;
+	}
+	public void OnYesClick()
+	{
+		Time.timeScale = 1;
+		GameControl.control.Save();
+	}
+	public void OnNoClick()
+	{
+		Time.timeScale = 1;
+		dialogueBoxUIObject.SetActive(false);
+	}
+
+}
