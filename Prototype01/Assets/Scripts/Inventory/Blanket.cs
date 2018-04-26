047ef3a00a4d3bfd2ecc8b476010eb47d3ebe907
paulRich <32309077+paulRich@users.noreply.github.com>
2018-04-26 17:18:21 -0500

    Blanket implementation

---
+++ Prototype01/Assets/Scripts/Inventory/Blanket.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * A script for blankets, which increase the player's maximum confidence
+ */
+
+public class Blanket : MaxConfidenceItem
+{
+    /**
+     * Initalization: Set the name and maximum confidence increase
+     */
+
+    public void Start()
+    {
+        myName = "Blanket";
+        bonusConfidence = 50;
+        description = "Increases maximum confidence by " + bonusConfidence;
+    }
+}
---
+++ Prototype01/Assets/Scripts/Inventory/MaxConfidenceItem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Superclass for items that increase maximum confidence
+ */
+
+public abstract class MaxConfidenceItem : Item
+{
+    /**
+     * How much maximum confidence should be increased by
+     */
+    protected int bonusConfidence;
+
+    /**
+     * Use the MaxConfidenceItem - increases the player's max confidence
+     */
+    protected override void UseAction()
+    {
+        Debug.Log("A " + myName + " was used. The player's maximum confidence should be increased by " + bonusConfidence + ".");
+        GameControl.control.AdjustMaxConfidenceBy(bonusConfidence);
+    }
+}
