8a4eb3a9532eda880cca22e0ed5d85dbfbe83071
lukepenney <luke.penney@my.wheaton.edu>
2018-04-26 21:03:27 -0500

    Added new title screen

--- Prototype01/Assets/Scripts/TitleScreen.cs
+++ Prototype01/Assets/Scripts/TitleScreen.cs
@@ -2,26 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /**
- * Script for controlling what happens in the TitleScreen scene
+ *Script for controlling what happens in the TitleScreen scene
  */
-
 public class TitleScreen : MonoBehaviour
 {
-	/**
-	 * Start a new game
+    /**
+	 *Start a new game
 	 */
 	public void NewGame()
 	{
-		SceneManager.LoadScene("Room01");
+        SceneManager.LoadScene("Room01");
 	}
 
 	/**
-	 * Load a saved game
-	 */
-	public void ResumeGame()
+	*Load a saved game
+	*/
+    public void ResumeGame()
 	{
-		GameControl.LoadNew();
+        GameControl.LoadNew();
 	}
 }
